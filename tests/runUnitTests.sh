28b454a55aac32023e8b6431be043d96df0e60bb
Aleksandr.Iakhnev <Aleksandr.Iakhnev@software.dell.com>
2016-11-29 12:19:39 +0300

    add test docker

--- tests/runUnitTests.sh
+++ tests/runUnitTests.sh
@@ -1,1 +1,5 @@
-docker run -v $(pwd)/..:/waferslim
+#!/usr/bin/env bash
+docker run -v $(pwd)/..:/waferslim -w /waferslim waferslim-unittests \
+    nosetests --verbose \
+    -with-coverage --cover-erase --cover-xml \
+    --cover-xml-file=test-results/coverage.xml --cover-package waferslim
