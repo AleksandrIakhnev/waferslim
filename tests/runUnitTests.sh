3367c60542ca8782af34cf92d5ab2c37ca3b5e13
Aleksadr.Iakhnev <Aleksadr.Iakhnev@software.dell.com>
2016-11-29 09:29:41 +0000

    fixing unit tests

--- tests/runUnitTests.sh
+++ tests/runUnitTests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker run -v $(pwd)/..:/waferslim -w /waferslim waferslim-unittests \
+docker run -v $(pwd)/..:/waferslim -v $(pwd)/..:/usr/local/lib/python3.5/site-packages/waferslim \
+    -w /waferslim/tests waferslim-unittests \
     nosetests --verbose \
-    -with-coverage --cover-erase --cover-xml \
+    --with-coverage --cover-erase --cover-xml \
     --cover-xml-file=test-results/coverage.xml --cover-package waferslim
